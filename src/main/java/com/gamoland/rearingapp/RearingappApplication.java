14e463c74141707aa27cb237f0ea55ece7d967ba
gamoland <szelenyim@gmail.com>
2021-04-29 20:18:51 +0200

    start project

---
+++ src/main/java/com/gamoland/rearingapp/RearingappApplication.java
@@ -0,0 +1,13 @@
+package com.gamoland.rearingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RearingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RearingappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gamoland/rearingapp/RearingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamoland.rearingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RearingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
